b0bdd52b3b40d8dcd4c15438124e9d0362ed3cfd
Julius0594 <158364861+JuliusM94@users.noreply.github.com>
2024-07-18 20:56:27 -0400

    ADDED JS LIBRARIES
    GLIDE, LIGHTBOX AND LEAFLET

--- public/assets/javascript/script.js
+++ public/assets/javascript/script.js
@@ -16,3 +16,20 @@ function initCarousel() {
 }
 
 document.addEventListener('DOMContentLoaded', initCarousel);
+
+// MAP | LEAFLET JS
+var map = L.map('map').setView([43.652798, -79.384055], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+// var marker = L.marker([43.651337, -79.384028]).addTo(map);
+
+var circle = L.circle([43.651337, -79.384028], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.3,
+    radius: 2000
+}).addTo(map);
